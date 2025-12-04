53fa96938c181cae53be4c0d32e82f753a6de2fb
Christopher Albert <albert@tugraz.at>
2025-12-04 19:33:15 +0100

    feat: Unify mesh plotting for triangles, quads, and mixed meshes

--- test/test_mesh_plotting.f90
+++ test/test_mesh_plotting.f90
@@ -12,6 +12,8 @@ program test_mesh_plotting
     call test_plot_function_exists()
     call test_simple_mesh_plot()
     call test_plot_with_options()
+    call test_quad_mesh_plot()
+    call test_mixed_mesh_plot()
     
     ! Summary
     write(*,*) ""
@@ -38,6 +40,66 @@ contains
         write(*,*) "  Testing basic plot interface..."
         call plot(mesh)  ! Should generate mesh plot
         write(*,*) "  ✓ plot(mesh) interface works"
+        
+        call end_test()
+    end subroutine
+    
+    subroutine test_quad_mesh_plot()
+        character(len=*), parameter :: test_name = "Quadrilateral Mesh Plot"
+        type(mesh_t) :: mesh
+        
+        call start_test(test_name)
+        
+        mesh = structured_quad_mesh(4, 3, 0.0_dp, 2.0_dp, 0.0_dp, 1.0_dp)
+        
+        write(*,*) "  Testing quadrilateral mesh plotting..."
+        call plot(mesh, filename="build/test_quad_mesh.png", &
+                  title="Structured Quad Mesh")
+        write(*,*) "  ✓ plot(quad mesh) works"
+        
+        call end_test()
+    end subroutine
+
+    subroutine test_mixed_mesh_plot()
+        character(len=*), parameter :: test_name = "Mixed Mesh Plot"
+        type(mesh_t) :: tri_mesh, quad_mesh, mixed_mesh
+        integer :: n_vertices, i
+        
+        call start_test(test_name)
+        
+        tri_mesh = unit_square_mesh(3)
+        quad_mesh = structured_quad_mesh(2, 2, 0.0_dp, 1.0_dp, 0.0_dp, 1.0_dp)
+        
+        mixed_mesh%data%n_vertices = tri_mesh%data%n_vertices + quad_mesh%data%n_vertices
+        mixed_mesh%data%n_triangles = tri_mesh%data%n_triangles
+        mixed_mesh%data%n_quads = quad_mesh%data%n_quads
+        mixed_mesh%data%has_triangles = .true.
+        mixed_mesh%data%has_quads = .true.
+        mixed_mesh%data%has_mixed_elements = .true.
+        
+        allocate(mixed_mesh%data%vertices(2, mixed_mesh%data%n_vertices))
+        allocate(mixed_mesh%data%triangles(3, mixed_mesh%data%n_triangles))
+        allocate(mixed_mesh%data%quads(4, mixed_mesh%data%n_quads))
+        
+        mixed_mesh%data%vertices(:, 1:tri_mesh%data%n_vertices) = tri_mesh%data%vertices
+        mixed_mesh%data%triangles = tri_mesh%data%triangles
+        
+        n_vertices = tri_mesh%data%n_vertices
+        do i = 1, quad_mesh%data%n_vertices
+            mixed_mesh%data%vertices(:, n_vertices + i) = quad_mesh%data%vertices(:, i) + &
+                [1.0_dp, 0.0_dp]
+        end do
+        do i = 1, quad_mesh%data%n_quads
+            mixed_mesh%data%quads(:, i) = quad_mesh%data%quads(:, i) + n_vertices
+        end do
+        
+        call mixed_mesh%data%build_connectivity()
+        call mixed_mesh%data%find_boundary()
+        
+        write(*,*) "  Testing mixed tri/quad mesh plotting..."
+        call plot(mixed_mesh, filename="build/test_mixed_mesh.png", &
+                  title="Mixed Tri/Quad Mesh")
+        write(*,*) "  ✓ plot(mixed mesh) works"
         
         call end_test()
     end subroutine
