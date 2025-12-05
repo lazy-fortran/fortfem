d55809436d394a9d005717269639d81bc5d3b823
Christopher Albert <albert@tugraz.at>
2025-12-05 13:24:37 +0100

    fix: initialize GMRES guess and refactor P2 assembly
    - Initialize x=0 in solve_curl_curl_problem before GMRES call to fix
      numerical divergence (regression from module split)
    - Refactor assemble_p2_laplacian_system from 113 to 48 lines
    - Extract compute_p2_element_matrices (43 lines) for element-local work
    - Extract apply_p2_dirichlet_bc (27 lines) for boundary conditions
    - All functions now within 100-line hard limit

--- src/fortfem_api_solvers.f90
+++ src/fortfem_api_solvers.f90
@@ -1,5 +1,6 @@
 module fortfem_api_solvers
     use fortfem_kinds
+    use fortfem_mesh_2d, only: mesh_2d_t
     use fortfem_api_types, only: function_space_t, function_t,              &
         vector_function_t, vector_function_space_t, dirichlet_bc_t,        &
         vector_bc_t, neumann_bc_t
@@ -368,20 +369,12 @@ contains
         type(dirichlet_bc_t), intent(in) :: bc
         real(dp), intent(inout) :: K(:,:), F(:)
 
-        integer :: ndof, i, j, kq, e, v1, v2, v3
-        real(dp) :: x1, y1, x2, y2, x3, y3, area
+        integer :: ndof, i, j, e, v1, v2, v3
         real(dp) :: K_elem(6,6), F_elem(6)
         integer :: dofs(6), edge1, edge2, edge3
-        type(basis_p2_2d_t) :: basis_p2
-        real(dp) :: xi_quad(3), eta_quad(3), w_quad(3)
-        real(dp) :: grad_i(2), grad_j(2), jac(2,2), det_j, inv_jac(2,2)
         real(dp) :: vertices(2,3)
 
         ndof = uh%space%ndof
-
-        xi_quad = [1.0_dp/6.0_dp, 2.0_dp/3.0_dp, 1.0_dp/6.0_dp]
-        eta_quad = [1.0_dp/6.0_dp, 1.0_dp/6.0_dp, 2.0_dp/3.0_dp]
-        w_quad = [1.0_dp/3.0_dp, 1.0_dp/3.0_dp, 1.0_dp/3.0_dp]
 
         do e = 1, uh%space%mesh%data%n_triangles
             v1 = uh%space%mesh%data%triangles(1, e)
@@ -395,54 +388,14 @@ contains
             vertices(1,3) = uh%space%mesh%data%vertices(1, v3)
             vertices(2,3) = uh%space%mesh%data%vertices(2, v3)
 
-            area = 0.5_dp * abs((vertices(1,2)-vertices(1,1))                &
-                *(vertices(2,3)-vertices(2,1))                                &
-                - (vertices(1,3)-vertices(1,1))                               &
-                *(vertices(2,2)-vertices(2,1)))
-
-            K_elem = 0.0_dp
-            F_elem = 0.0_dp
-
-            dofs(1) = v1
-            dofs(2) = v2
-            dofs(3) = v3
-
+            dofs(1:3) = [v1, v2, v3]
             call find_triangle_edges(uh%space%mesh%data, e, edge1, edge2,   &
                                      edge3)
-
             dofs(4) = uh%space%mesh%data%n_vertices + edge1
             dofs(5) = uh%space%mesh%data%n_vertices + edge2
             dofs(6) = uh%space%mesh%data%n_vertices + edge3
 
-            call basis_p2%compute_jacobian(vertices, jac, det_j)
-
-            inv_jac(1,1) = jac(2,2) / det_j
-            inv_jac(1,2) = -jac(1,2) / det_j
-            inv_jac(2,1) = -jac(2,1) / det_j
-            inv_jac(2,2) = jac(1,1) / det_j
-
-            do i = 1, 6
-                do j = 1, 6
-                    do kq = 1, 3
-                        grad_i = basis_p2%grad(i, xi_quad(kq),              &
-                                               eta_quad(kq))
-                        grad_j = basis_p2%grad(j, xi_quad(kq),              &
-                                               eta_quad(kq))
-
-                        grad_i = matmul(inv_jac, grad_i)
-                        grad_j = matmul(inv_jac, grad_j)
-
-                        K_elem(i,j) = K_elem(i,j) + w_quad(kq) *            &
-                            (grad_i(1)*grad_j(1) + grad_i(2)*grad_j(2)) *   &
-                            area
-                    end do
-                end do
-
-                do kq = 1, 3
-                    F_elem(i) = F_elem(i) + w_quad(kq) *                    &
-                        basis_p2%eval(i, xi_quad(kq), eta_quad(kq)) * area
-                end do
-            end do
+            call compute_p2_element_matrices(vertices, K_elem, F_elem)
 
             do i = 1, 6
                 if (dofs(i) > 0 .and. dofs(i) <= ndof) then
@@ -457,25 +410,81 @@ contains
             end do
         end do
 
-        do i = 1, uh%space%mesh%data%n_vertices
-            if (uh%space%mesh%data%is_boundary_vertex(i)) then
+        call apply_p2_dirichlet_bc(uh%space%mesh%data, bc%value, ndof, K, F)
+    end subroutine assemble_p2_laplacian_system
+
+    subroutine compute_p2_element_matrices(vertices, K_elem, F_elem)
+        real(dp), intent(in) :: vertices(2,3)
+        real(dp), intent(out) :: K_elem(6,6), F_elem(6)
+
+        type(basis_p2_2d_t) :: basis_p2
+        real(dp) :: xi_q(3), eta_q(3), w_q(3)
+        real(dp) :: jac(2,2), det_j, inv_jac(2,2), area
+        real(dp) :: grad_i(2), grad_j(2)
+        integer :: i, j, kq
+
+        xi_q = [1.0_dp/6.0_dp, 2.0_dp/3.0_dp, 1.0_dp/6.0_dp]
+        eta_q = [1.0_dp/6.0_dp, 1.0_dp/6.0_dp, 2.0_dp/3.0_dp]
+        w_q = [1.0_dp/3.0_dp, 1.0_dp/3.0_dp, 1.0_dp/3.0_dp]
+
+        area = 0.5_dp * abs((vertices(1,2)-vertices(1,1))                    &
+            *(vertices(2,3)-vertices(2,1))                                    &
+            - (vertices(1,3)-vertices(1,1))*(vertices(2,2)-vertices(2,1)))
+
+        call basis_p2%compute_jacobian(vertices, jac, det_j)
+        inv_jac(1,1) = jac(2,2) / det_j
+        inv_jac(1,2) = -jac(1,2) / det_j
+        inv_jac(2,1) = -jac(2,1) / det_j
+        inv_jac(2,2) = jac(1,1) / det_j
+
+        K_elem = 0.0_dp
+        F_elem = 0.0_dp
+
+        do i = 1, 6
+            do j = 1, 6
+                do kq = 1, 3
+                    grad_i = matmul(inv_jac,                                 &
+                        basis_p2%grad(i, xi_q(kq), eta_q(kq)))
+                    grad_j = matmul(inv_jac,                                 &
+                        basis_p2%grad(j, xi_q(kq), eta_q(kq)))
+                    K_elem(i,j) = K_elem(i,j) + w_q(kq) * area *             &
+                        (grad_i(1)*grad_j(1) + grad_i(2)*grad_j(2))
+                end do
+            end do
+            do kq = 1, 3
+                F_elem(i) = F_elem(i) + w_q(kq) * area *                     &
+                    basis_p2%eval(i, xi_q(kq), eta_q(kq))
+            end do
+        end do
+    end subroutine compute_p2_element_matrices
+
+    subroutine apply_p2_dirichlet_bc(mdata, bc_value, ndof, K, F)
+        type(mesh_2d_t), intent(in) :: mdata
+        real(dp), intent(in) :: bc_value
+        integer, intent(in) :: ndof
+        real(dp), intent(inout) :: K(:,:), F(:)
+
+        integer :: i, j
+
+        do i = 1, mdata%n_vertices
+            if (mdata%is_boundary_vertex(i)) then
                 K(i,:) = 0.0_dp
                 K(i,i) = 1.0_dp
-                F(i) = bc%value
+                F(i) = bc_value
             end if
         end do
 
-        do i = 1, uh%space%mesh%data%n_edges
-            if (uh%space%mesh%data%is_boundary_edge(i)) then
-                j = uh%space%mesh%data%n_vertices + i
+        do i = 1, mdata%n_edges
+            if (mdata%is_boundary_edge(i)) then
+                j = mdata%n_vertices + i
                 if (j <= ndof) then
                     K(j,:) = 0.0_dp
                     K(j,j) = 1.0_dp
-                    F(j) = bc%value
+                    F(j) = bc_value
                 end if
             end if
         end do
-    end subroutine assemble_p2_laplacian_system
+    end subroutine apply_p2_dirichlet_bc
 
     subroutine solve_generic_problem(uh, bc)
         type(function_t), intent(inout) :: uh
@@ -690,6 +699,7 @@ contains
 
         ndof = Eh%space%ndof
         allocate(A(ndof, ndof), b(ndof), x(ndof))
+        x = 0.0_dp
 
         call assemble_curl_curl_system(Eh, A, b)
 
