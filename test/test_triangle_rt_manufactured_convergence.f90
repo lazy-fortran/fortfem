ebc7982f7bcfbe81d053380f3aa2bc073ff0b70d
Christopher Albert <albert@tugraz.at>
2026-07-29 20:56:36 +0200

    test: verify arbitrary-order Raviart-Thomas convergence

---
+++ test/test_triangle_rt_manufactured_convergence.f90
@@ -0,0 +1,120 @@
+program test_triangle_rt_manufactured_convergence
+    use check, only: check_condition, check_summary
+    use fortfem_api, only: &
+        evaluate_triangle_rt_interpolant, initialize_triangle_raviart_thomas, &
+        interpolate_triangle_rt, triangle_duffy_quadrature, &
+        triangle_rt_basis_t
+    use fortfem_api_mesh, only: mesh_t, unit_square_mesh
+    use fortfem_kinds, only: dp
+    implicit none
+
+    real(dp) :: coarse_divergence_error, coarse_l2_error
+    real(dp) :: divergence_rate, fine_divergence_error, fine_l2_error, l2_rate
+    integer :: degree, order
+    logical :: all_passed
+
+    all_passed = .true.
+    do degree = 0, 3
+        order = degree + 1
+        call interpolation_errors( &
+            3, degree, coarse_l2_error, coarse_divergence_error)
+        call interpolation_errors( &
+            5, degree, fine_l2_error, fine_divergence_error)
+        l2_rate = log(coarse_l2_error / fine_l2_error) / log(2.0_dp)
+        divergence_rate = log( &
+            coarse_divergence_error / fine_divergence_error) / log(2.0_dp)
+        call record_condition(fine_l2_error < coarse_l2_error .and. &
+            fine_divergence_error < coarse_divergence_error, &
+            "Manufactured H(div) interpolation improves under refinement")
+        call record_condition(l2_rate > real(order, dp) - 0.35_dp, &
+            "Manufactured flux-field error reaches the expected h-order")
+        call record_condition( &
+            divergence_rate > real(order, dp) - 0.45_dp, &
+            "Manufactured divergence error reaches the expected h-order")
+    end do
+
+    call check_summary("Arbitrary-order RT manufactured convergence")
+    if (.not. all_passed) error stop 1
+
+contains
+
+    subroutine interpolation_errors( &
+            vertex_count, degree, l2_error, divergence_error)
+        integer, intent(in) :: vertex_count, degree
+        real(dp), intent(out) :: l2_error, divergence_error
+
+        type(mesh_t) :: mesh
+        type(triangle_rt_basis_t) :: basis
+        real(dp), allocatable :: dofs(:), eta(:), weights(:), xi(:)
+        real(dp) :: determinant, exact_divergence, exact_value(2)
+        real(dp) :: interpolated_divergence, interpolated_value(2)
+        real(dp) :: jacobian(2, 2), physical_point(2), vertices(2, 3)
+        integer :: point, quadrature_degree, status, triangle
+
+        mesh = unit_square_mesh(vertex_count)
+        quadrature_degree = 2 * degree + 8
+        call initialize_triangle_raviart_thomas(degree, basis, status)
+        call triangle_duffy_quadrature( &
+            quadrature_degree, xi, eta, weights, status)
+        l2_error = 0.0_dp
+        divergence_error = 0.0_dp
+        do triangle = 1, mesh%data%n_triangles
+            vertices = mesh%data%vertices(:, mesh%data%triangles(:, triangle))
+            jacobian(:, 1) = vertices(:, 2) - vertices(:, 1)
+            jacobian(:, 2) = vertices(:, 3) - vertices(:, 1)
+            determinant = jacobian(1, 1) * jacobian(2, 2) - &
+                jacobian(1, 2) * jacobian(2, 1)
+            call interpolate_triangle_rt( &
+                vertices, degree, quadrature_degree, manufactured_field, &
+                dofs, status)
+            do point = 1, size(weights)
+                physical_point = vertices(:, 1) + &
+                    matmul(jacobian, [xi(point), eta(point)])
+                call manufactured_field( &
+                    physical_point(1), physical_point(2), exact_value)
+                exact_divergence = manufactured_divergence( &
+                    physical_point(1), physical_point(2))
+                call evaluate_triangle_rt_interpolant( &
+                    vertices, basis, dofs, xi(point), eta(point), &
+                    interpolated_value, interpolated_divergence, status)
+                l2_error = l2_error + determinant * weights(point) * &
+                    sum((interpolated_value - exact_value)**2)
+                divergence_error = divergence_error + &
+                    determinant * weights(point) * &
+                    (interpolated_divergence - exact_divergence)**2
+            end do
+            deallocate(dofs)
+        end do
+        l2_error = sqrt(l2_error)
+        divergence_error = sqrt(divergence_error)
+        call mesh%destroy()
+    end subroutine interpolation_errors
+
+    pure subroutine manufactured_field(x, y, value)
+        real(dp), intent(in) :: x, y
+        real(dp), intent(out) :: value(2)
+
+        real(dp), parameter :: pi = acos(-1.0_dp)
+
+        value(1) = sin(pi * x) * sin(pi * y)
+        value(2) = 0.0_dp
+    end subroutine manufactured_field
+
+    pure function manufactured_divergence(x, y) result(value)
+        real(dp), intent(in) :: x, y
+        real(dp) :: value
+
+        real(dp), parameter :: pi = acos(-1.0_dp)
+
+        value = pi * cos(pi * x) * sin(pi * y)
+    end function manufactured_divergence
+
+    subroutine record_condition(condition, description)
+        logical, intent(in) :: condition
+        character(len=*), intent(in) :: description
+
+        all_passed = all_passed .and. condition
+        call check_condition(condition, description)
+    end subroutine record_condition
+
+end program test_triangle_rt_manufactured_convergence
